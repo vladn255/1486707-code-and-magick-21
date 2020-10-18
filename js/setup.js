28b623dc92d985b9666610b4c80ac61c7af07c0d
vladn255 <nesvitvlad@yandex.ru>
2020-10-18 18:00:41 +0400

    9. Учебный проект: одеть Надежду

--- js/setup.js
+++ js/setup.js
@@ -5,10 +5,19 @@ const NAMES_LIST = [`Иван`, `Хуан Себастьян`, `Мария`, `К
 const SURNAMES_LIST = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
 const COAT_COLORS_LIST = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
 const EYES_COLORS_LIST = [`black`, `red`, `blue`, `yellow`, `green`];
+const FIREBALL_COLORS_LIST = [`#ee4830`, `#30a8ee`, `#5ce6c0`, `#e848d5`, `#e6e848`];
 const similarListElement = setupBlock.querySelector(`.setup-similar-list`);
 const similarWizardTemplate = document.querySelector(`#similar-wizard-template`)
-.content
-.querySelector(`.setup-similar-item`);
+  .content
+  .querySelector(`.setup-similar-item`);
+const MIN_NAME_LENGTH = 2;
+const MAX_NAME_LENGTH = 25;
+const userNameInput = document.querySelector(`.setup-user-name`);
+const setupPlayer = document.querySelector(`.setup-player`);
+const setupWizard = document.querySelector(`.setup-wizard`);
+const wizardCoat = setupWizard.querySelector(`.wizard-coat`);
+const wizardEyes = setupWizard.querySelector(`.wizard-eyes`);
+const wizardFireball = document.querySelector(`.setup-fireball-wrap`);
 
 const charactersList = [];
 
@@ -30,6 +39,11 @@ const createCoatColor = function () {
 const createEyesColor = function () {
   const eyesColor = EYES_COLORS_LIST[randomInteger(0, EYES_COLORS_LIST.length - 1)];
   return eyesColor;
+};
+
+const createFireballColor = function () {
+  const fireballColor = FIREBALL_COLORS_LIST[randomInteger(0, FIREBALL_COLORS_LIST.length - 1)];
+  return fireballColor;
 };
 
 const createCharactersList = function () {
@@ -61,7 +75,91 @@ for (let i = 0; i < charactersList.length; i++) {
   fragment.appendChild(renderWizard(charactersList[i]));
 }
 
+const setupOpen = document.querySelector(`.setup-open`);
+const setup = document.querySelector(`.setup`);
+const setupClose = setup.querySelector(`.setup-close`);
+
+const onPopupEscPress = function (evt) {
+  if (evt.key === `Escape`) {
+    evt.preventDefault();
+    if (!evt.target.matches(`.setup-user-name`)) {
+      closePopup();
+    }
+  }
+};
+
+const openPopup = function () {
+  setup.classList.remove(`hidden`);
+  document.addEventListener(`keydown`, onPopupEscPress);
+};
+
+const closePopup = function () {
+  setup.classList.add(`hidden`);
+  document.removeEventListener(`keydown`, onPopupEscPress);
+};
+
+const coatChanger = function () {
+  wizardCoat.style.fill = createCoatColor();
+  setupPlayer.querySelector(`input[name="coat-color"]`).value = wizardCoat.style.fill;
+};
+
+const eyesChanger = function () {
+  wizardEyes.style.fill = createEyesColor();
+  setupPlayer.querySelector(`input[name="eyes-color"]`).value = wizardEyes.style.fill;
+};
+
+const fireballChanger = function () {
+  wizardFireball.querySelector(`input`).value = createFireballColor();
+  wizardFireball.querySelector(`.setup-fireball`).style.backgroundColor = wizardFireball.querySelector(`input`).value;
+};
+
+setupOpen.addEventListener(`click`, function () {
+  openPopup();
+});
+
+setupOpen.addEventListener(`keydown`, function (evt) {
+  if (evt.key === `Enter`) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener(`click`, function () {
+  closePopup();
+});
+
+setupClose.addEventListener(`keydown`, function (evt) {
+  if (evt.key === `Enter`) {
+    closePopup();
+  }
+});
+
+userNameInput.addEventListener(`input`, function () {
+  const valueLength = userNameInput.value.length;
+
+  if (valueLength < MIN_NAME_LENGTH) {
+    userNameInput.setCustomValidity(`Ещё ` + (MIN_NAME_LENGTH - valueLength) + ` симв.`);
+  } else if (valueLength > MAX_NAME_LENGTH) {
+    userNameInput.setCustomValidity(`Удалите лишние ` + (valueLength - MAX_NAME_LENGTH) + ` симв.`);
+  } else {
+    userNameInput.setCustomValidity(``);
+  }
+  userNameInput.reportValidity();
+});
+
+wizardCoat.addEventListener(`click`, function () {
+  coatChanger();
+});
+
+wizardEyes.addEventListener(`click`, function () {
+  eyesChanger();
+});
+
+wizardFireball.addEventListener(`click`, function () {
+  fireballChanger();
+});
+
 similarListElement.appendChild(fragment);
 
 setupBlock.classList.remove(`hidden`);
 setupBlock.querySelector(`.setup-similar`).classList.remove(`hidden`);
+
